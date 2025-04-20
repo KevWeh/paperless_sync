14be356ca030744b72cbeadd1d87177414d57e36
Kevin <kevweh89@gmail.com>
2025-04-20 17:24:36 +0200

    Create paperless_sync.sh

---
+++ paperless_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#####################################################################################
+
+# Script Name	:	paperless_sync
+# Description	:	Sync archive documents from Paperless-ngx with mounted archive folder
+# Args		:
+# Author	:	Kevin Wehrli
+# Email		:	-
+
+#####################################################################################
